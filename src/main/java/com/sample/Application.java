f44fd5a586e700b378a9a144baad7c88199f41ed
Philippe Brossier <brossierp@gmail.com>
2020-08-16 13:06:15 +0100

    tidy up

--- src/main/java/com/sample/Application.java
+++ src/main/java/com/sample/Application.java
@@ -1,16 +1,19 @@
 package com.sample;
 
 import com.sample.client.GreetingWebClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
         GreetingWebClient gwc = new GreetingWebClient();
-        System.out.println(gwc.getResult());
+        LOGGER.debug("Response obtained using the GreetingWebClient is {}", gwc.getResult());
     }
 }
