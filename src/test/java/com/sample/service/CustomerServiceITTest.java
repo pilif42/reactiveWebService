a7ca2f9d6810b4f6858fe641f105e51e55b4a9f4
Philippe Brossier <brossierp@gmail.com>
2020-09-20 10:10:46 +0100

    TODO for next time

--- src/test/java/com/sample/service/CustomerServiceITTest.java
+++ src/test/java/com/sample/service/CustomerServiceITTest.java
@@ -5,12 +5,17 @@ import com.sample.db.repository.ReactiveCustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.function.Predicate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
+
+@DirtiesContext(classMode = BEFORE_CLASS)
 @SpringBootTest
 public class CustomerServiceITTest {
     @Autowired
@@ -34,7 +39,7 @@ public class CustomerServiceITTest {
                 .expectNextMatches(saved -> saved.getEmail().equals(emailAddress) &&
                         saved.getPassword().equals(password) &&
                         saved.getRole().equals(role) &&
-                        saved.getId() > 0)
+                        saved.getId() == 5) // We expect 5 as 4 customers are stored previously by SampleDataInitializer.
                 .verifyComplete();
     }
 
@@ -48,14 +53,28 @@ public class CustomerServiceITTest {
         Predicate<Customer> match = customer -> saved.any(saveItem -> saveItem.equals(customer)).block();
 
         // WHEN
-        Flux<Customer> composite = customerService.findAll().thenMany(saved);
+        Mono<Long> composite = customerService.findAll().count();
+
+        Long nbSavedCustomers = composite.block();
+        assertEquals(4, nbSavedCustomers);
+
+        /**
+         * TODO The below was what was found in the tutorial. It does not seem to be a good test though.
+         * TODO The 4 on L59 is our 4 customers stored previously by SampleDataInitializer. Instead, we want to assert
+         * TODO on the ones created at L49.
+         *
+         * TODO Check what the thenMany really does as it seems that the test returns saved on findAll() to then assert
+         * TODO that it is saved. Rather pointless. Instead, we should ensure that the saving done on L49 does work.
+         */
+
+//        Flux<Customer> composite = customerService.findAll().thenMany(saved);
 
         // THEN
-        StepVerifier
-                .create(composite)
-                .expectNextMatches(match)
-                .expectNextMatches(match)
-                .expectNextMatches(match)
-                .verifyComplete();
+//        StepVerifier
+//                .create(composite)
+//                .expectNextMatches(match)
+//                .expectNextMatches(match)
+//                .expectNextMatches(match)
+//                .verifyComplete();
     }
 }
