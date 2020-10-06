347eb311863ac4fb31ea55fb15e34e7770d405fe
Philippe Brossier <brossierp@gmail.com>
2020-10-06 21:39:17 +0100

    progressing unit tests

--- src/test/java/com/sample/router/CustomerRouterITTest.java
+++ src/test/java/com/sample/router/CustomerRouterITTest.java
@@ -6,6 +6,14 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.Matchers.equalTo;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @WithMockUser
 public class CustomerRouterITTest {
@@ -15,6 +23,34 @@ public class CustomerRouterITTest {
 
     @Test
     public void testGetAllCustomers() {
+        // GIVEN
+        Map customer1 = new LinkedHashMap();
+        customer1.put("id", 1);
+        customer1.put("email", "A@email.com");
+        customer1.put("password", "tester123");
+        customer1.put("role", "Tester");
+
+        Map customer2 = new LinkedHashMap();
+        customer2.put("id", 2);
+        customer2.put("email", "B@email.com");
+        customer2.put("password", "tester123");
+        customer2.put("role", "Tester");
+
+        Map customer3 = new LinkedHashMap();
+        customer3.put("id", 3);
+        customer3.put("email", "C@email.com");
+        customer3.put("password", "tester123");
+        customer3.put("role", "Tester");
+
+        Map customer4 = new LinkedHashMap();
+        customer4.put("id", 4);
+        customer4.put("email", "D@email.com");
+        customer4.put("password", "tester123");
+        customer4.put("role", "Tester");
+
+        List<Map> expectedList = Stream.of(customer1, customer2, customer3, customer4).collect(Collectors.toList());
+
+        // WHEN & THEN
         WebTestClient.bindToServer()
                 .baseUrl("http://localhost:" + port)
                 .build()
@@ -22,9 +58,32 @@ public class CustomerRouterITTest {
                 .uri("/customers")
                 .exchange()
                 .expectStatus()
-                .isOk();
-
-        // TODO .expectBody() = le below
-        // TODO [{"id":1,"email":"A@email.com","password":"tester123","role":"Tester"},{"id":2,"email":"B@email.com","password":"tester123","role":"Tester"},{"id":3,"email":"C@email.com","password":"tester123","role":"Tester"},{"id":4,"email":"D@email.com","password":"tester123","role":"Tester"}]
+                .isOk().expectBody(List.class)
+                .value(List::size, equalTo(4))
+                .value(list -> list.containsAll(expectedList), equalTo(true));
     }
+
+    @Test
+    public void testGetOneCustomer() {
+        // GIVEN
+        Map customer = new LinkedHashMap();
+        customer.put("id", 3);
+        customer.put("email", "C@email.com");
+        customer.put("password", "tester123");
+        customer.put("role", "Tester");
+
+        // WHEN & THEN
+        WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .build()
+                .get()
+                .uri("/customers/3")
+                .exchange()
+                .expectStatus()
+                .isOk().expectBody(Map.class)
+                .value(map -> map.equals(customer), equalTo(true));
+    }
+
+    // TODO Test for Get /customers/a -> currently 500 as we blow up in findOne of CustomerService. Should be 400 - id is an integer.
+    // TODO Tests for Post
 }
